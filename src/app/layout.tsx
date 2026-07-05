8e60cdbd6738d069fa794e4d1d2dc24c361185c1
Asif Afroj Shayon <shayon.asif@gmail.com>
2026-07-05 11:56:07 +0600

    feat: add SolaimanLipi font and update global styles
    - Introduced custom font "SolaimanLipi" for both normal and bold weights.
    - Updated global CSS variables for light and dark modes.
    - Replaced previous font settings with "SolaimanLipi" for body and utility classes.
    - Removed unused Google fonts from layout component to streamline imports.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import ThemeProvider from "@/components/ThemeProvider";
 import Header from "@/components/Header";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "RTV Bangladesh | Source for the Latest News & Entertainment",
@@ -28,11 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="bn"
-      suppressHydrationWarning
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang="bn" suppressHydrationWarning className="h-full">
       <body className="min-h-full flex flex-col">
         <ThemeProvider>
           <Header />
