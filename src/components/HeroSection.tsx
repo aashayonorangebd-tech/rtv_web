0dd2c0c2b5485365c616d67511220ee8f61fc481
Asif Afroj Shayon <shayon.asif@gmail.com>
2026-07-05 15:30:30 +0600

    refactor: update HeroSection layout for improved responsiveness and consistency

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -8,12 +8,12 @@ export default function HeroSection({ stories }: { stories: StoryModel[] }) {
   const rightColStories = rest.slice(0, 2);
 
   return (
-    <section className="grid grid-cols-1 lg:grid-cols-4 gap-6 lg:gap-8 mb-10 mt-4">
-      <div className="lg:col-span-2 mb-8 lg:mb-0">
+    <section className="grid grid-cols-1 md:grid-cols-4 gap-6 md:gap-8 mb-10 mt-4">
+      <div className="md:col-span-2 mb-8 md:mb-0">
         <StoryCardLarge story={lead} />
       </div>
 
-      <div className="lg:col-span-1 flex flex-col justify-start lg:border-r border-[#dddddd] dark:border-gray-800 lg:pr-6">
+      <div className="md:col-span-1 flex flex-col justify-start md:border-r border-[#dddddd] dark:border-gray-800 lg:pr-6">
         {rightColStories.map((story) => (
           <StoryCardMedium key={story.storyId} story={story} />
         ))}
